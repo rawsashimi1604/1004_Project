458470ea8ff79aa91db68d7ef4e4dcecef765e11
DoubleBounce <cheekangchen@gmail.com>
2021-11-12 00:42:32 +0800

    Added carousel styles in Testbed/css/style.css, added carousel in index.php

--- Testbed-01/index.php
+++ Testbed-01/index.php
@@ -7,96 +7,116 @@ and open the template in the editor.
 
 
 <html lang="EN">
-<?php include "head.inc.php" ?>
+    <?php include "head.inc.php" ?>
 
-<?php include "DB_getall.inc.php" ?>
-<!-- BODY -->
+    <?php include "DB_getall.inc.php" ?>
+    <!-- BODY -->
 
-<body>
-    <?php include "nav.inc.php" ?>
-    <header class="jumbotron text-center bg-dark">
-        <h1 class="display-4 text-light">Steam Clone</h1>
-        <h2 class="text-light">Get ready to rumble</h2>
-    </header>
-
-    <!-- MAIN -->
-    <main class="container mt-5">
-        <section id="featured">
-            <h2 class="text-center">Featured Products</h2>
-            <p class="text-center">Checkout new and popular products</p>
-            <div class="row">
-                <article class="col-sm">
-                    <?php
-                    if ($result->num_rows > 0) {
-                        // Fetch all the results from our database
-                        while ($row = $result->fetch_assoc()) {
-                            echo '<figure><img class="img-thumbnail" src="' . $row["image"] . '" /><figcaption>' . $row["name"] . '</figcaption></figure><br />';
-                        }
-                    } else {
-                        $errorMsgDB = "Looks like there's nothing here..";
-                        $success = false;
-                    }
-                    ?>
-                </article>
-                <article class="col-sm">
-                    <?php
-                    echo '<div class="container"><row class="row-cols-2">';
-                    if ($result->num_rows > 0) {
-                        // Fetch all the results from our database
-                        while ($row = $result->fetch_assoc()) {
-                            echo '<div class="col"><figure><img class="img-thumbnail" src="' . $row["image"] . '" /><figcaption>' . $row["name"] . '</figcaption></figure></div>';
-                        }
-                    } else {
-                        $errorMsgDB = "Looks like there's nothing here..";
-                        $success = false;
-                    }
-                    echo "</row></div>";
-                    $stmt->close();
-                    $conn->close();
-
-                    ?>
-                </article>
+    <body>
+        <?php include "nav.inc.php" ?>
+        <header class="jumbotron text-center bg-dark">
+            <h1 class="display-4 text-light">Steam Clone</h1>
+            <h2 class="text-light">Get ready to rumble</h2>
+            <div class="game-images">
+                <div id="game-image-carousell" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#game-image-carousell" data-bs-slide-to="0" class="active"
+                                aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#game-image-carousell" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#game-image-carousell" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://cdn.akamai.steamstatic.com/steam/apps/10/header.jpg?t=1602535893"
+                                 class="d-block" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.akamai.steamstatic.com/steam/apps/220/header.jpg?t=1591063154"
+                                 class="d-block" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.akamai.steamstatic.com/steam/apps/240/header.jpg?t=1602536047"
+                                 class="d-block" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#game-image-carousell"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#game-image-carousell"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
+        </header>
 
-        </section>
-    </main>
-    
-    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="loginmodalTitle">Member Login</h5>
-            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form action="">
-              <!-- Login details -->
-              <div class="row login-row">
-                <div class="col">
-                  <label for="email" class="form-label">Enter your email address:</label>
-                  <input type="email" class="form-control" aria-label="First name" id="email">
+
+        <!-- MAIN -->
+        <main class="container mt-5">
+            <section id="featured">
+                <h2 class="text-center">Featured Products</h2>
+                <p class="text-center">Checkout new and popular products</p>
+                <div class="row">
+                    <article>
+                        <?php
+                        echo '<div class="container"><div class="row row-cols-3">';
+                        if ($result->num_rows > 0) {
+                            // Fetch all the results from our database
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<div class="col"><figure><img class="img-thumbnail" src="' . $row["image"] . '" /><figcaption>' . $row["name"] . '</figcaption></figure></div>';
+                            }
+                        } else {
+                            $errorMsgDB = "Looks like there's nothing here..";
+                            $success = false;
+                        }
+                        echo "</div></div>";
+                        ?>
+                    </article>
                 </div>
-              </div>
-              <div class="row login-row">
-                <div class="col">
-                  <label for="pwd" class="form-label">Enter your password:</label>
-                  <input type="password" class="form-control" aria-label="Last name" id="pwd">
+            </section>
+        </main>
+
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="loginmodalTitle">Member Login</h5>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="">
+                            <!-- Login details -->
+                            <div class="row login-row">
+                                <div class="col">
+                                    <label for="email" class="form-label">Enter your email address:</label>
+                                    <input type="email" class="form-control" aria-label="First name" id="email">
+                                </div>
+                            </div>
+                            <div class="row login-row">
+                                <div class="col">
+                                    <label for="pwd" class="form-label">Enter your password:</label>
+                                    <input type="password" class="form-control" aria-label="Last name" id="pwd">
+                                </div>
+
+                            </div>
+
+                            <button type="submit" class="btn btn-light login-btn">Login</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
-
-              </div>
-
-              <button type="submit" class="btn btn-light login-btn">Login</button>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <!-- FOOTER -->
-    <?php include "footer.inc.php" ?>
-</body>
+        <!-- FOOTER -->
+        <?php include "footer.inc.php" ?>
+    </body>
--- Testbed-01/nav.inc.php
+++ Testbed-01/nav.inc.php
@@ -1,33 +1,37 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-        aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link" href="./index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./games.php">Games</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./about.php">About</a>
-            </li>
+    <div class="d-flex collapse navbar-collapse" id="navbarToggler">
+      <div class="me-auto">
+        <ul class="navbar-nav p-2">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./games.php">Games</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./about.php">About</a>
+          </li>
         </ul>
-        <ul class="navbar-nav mt-2 mt-lg-0 justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link" href="./account.php">Account</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./register.php">Register</a>
-            </li>
-            <li class="nav-item">
-                <!-- <a class="nav-link" href="./login.php">Login</a> -->
-                <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#loginModal">
-                    Login
-                </button>
-            </li>
+      </div>
+      <div class="p-2">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="./account.php">Account</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./register.php">Register</a>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="./login.php">Login</a> -->
+            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#loginModal">
+              Login
+            </button>
+          </li>
         </ul>
+      </div>
     </div>
-</nav>
+  </nav>
