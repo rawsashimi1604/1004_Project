b8ce83129f7f7dee6f7cccba8441e56587bbe29e
Gavin Loo <looweiren@gmail.com>
2021-11-27 19:12:41 +0800

    Added responsive design for about.php

--- Testbed-01/about.php
+++ Testbed-01/about.php
@@ -9,30 +9,32 @@
             include "nav.inc.php";
         ?>
         <!-- MAIN -->
-        <main class="container bg-dark text-light">
+        <main class="container bg-dark text-light about-container">
             <div class="row">
-                <div class="col about-col" data-aos="fade-right" data-aos-duration="1500">
+                <div class="col-md about-col aos-init aos-animate" data-aos="fade-right" data-aos-duration="1500">
                     <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
                 </div>
-                <div class="col about-col" data-aos="fade-left" data-aos-duration="1500">
+                <div class="col-md about-col aos-init aos-animate" data-aos="fade-left" data-aos-duration="1500">
                     <p>We are a team of students from SIT.</p>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col about-col" data-aos="fade-right" data-aos-duration="1500">
+                <div class="col-md order-md-1 order-2 about-col aos-init aos-animate" data-aos="fade-right"
+                    data-aos-duration="1500">
                     <p>We are a team of students from SIT.</p>
                 </div>
-                <div class="col about-col" data-aos="fade-left" data-aos-duration="1500">
+                <div class="col-md order-md-2 order-1 about-col aos-init aos-animate" data-aos="fade-left"
+                    data-aos-duration="1500">
                     <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
                 </div>
             </div>
 
             <div class="row">
-                <div class="col about-col" data-aos="fade-right" data-aos-duration="1500">
+                <div class="col-md about-col aos-init aos-animate" data-aos="fade-right" data-aos-duration="1500">
                     <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
                 </div>
-                <div class="col about-col" data-aos="fade-left" data-aos-duration="1500">
+                <div class="col-md about-col aos-init aos-animate" data-aos="fade-left" data-aos-duration="1500">
                     <p>We are a team of students from SIT.</p>
                 </div>
             </div>
