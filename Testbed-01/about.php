fc7109194150873d5f3b5790375265f4484c2897
Gavin Loo <looweiren@gmail.com>
2021-11-29 18:51:07 +0800

    Added new about.php design

--- Testbed-01/about.php
+++ Testbed-01/about.php
@@ -10,32 +10,73 @@
         ?>
         <!-- MAIN -->
         <main class="container bg-dark text-light about-container">
-            <div class="row">
-                <div class="col-md about-col aos-init aos-animate" data-aos="fade-right" data-aos-duration="1500">
-                    <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
-                </div>
-                <div class="col-md about-col aos-init aos-animate" data-aos="fade-left" data-aos-duration="1500">
-                    <p>We are a team of students from SIT.</p>
+            <div class="about-wrapper">
+                <div class="content">
+                    <h1 class="about-header">Who we are</h1>
+                    <p class="about-author"><b>Yong Jing Kiang</b>, Chief Executive Officer</p>
+                    <p class="about-main-text">
+                        We are a team of students from Singapore Institute of Technology currently studying ICT1004 (Web
+                        Systems and Techonologies). We hope that through this project, we would be able to apply what we
+                        have learnt in class and improve our Web Development skills.
+                    </p>
+                    <p class="about-main-text">
+                        For our project, we decided to do a gaming ecommerce site as we were all passionate about video
+                        games.
+                    </p>
+                    <p class="about-main-text">
+                        The main technologies we used were:
+                    </p>
+                    <ul>
+                        <li>HTML -> HTML templating</li>
+                        <li>CSS -> Styling</li>
+                        <li>Bootstrap -> CSS library to accelarate styling process</li>
+                        <li>Vanilla JS -> Client-side scripting</li>
+                        <li>PHP -> Main Server-side technology</li>
+                        <li>Google Cloud -> Cloud hosting service</li>
+                        <li>Apache -> Web server software</li>
+                        <li>MySQL -> Database management system (DBMS)</li>
+                    </ul>
+                    <p class="about-main-text">
+                        Visit our <a target="_blank" href="https://github.com/rawsashimi1604/1004_Project">Github Page</a>
+                        to view our
+                        source code!
+                    </p>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-md order-md-1 order-2 about-col aos-init aos-animate" data-aos="fade-right"
-                    data-aos-duration="1500">
-                    <p>We are a team of students from SIT.</p>
-                </div>
-                <div class="col-md order-md-2 order-1 about-col aos-init aos-animate" data-aos="fade-left"
-                    data-aos-duration="1500">
-                    <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md about-col aos-init aos-animate" data-aos="fade-right" data-aos-duration="1500">
-                    <img src="./images/about_game.jpg" alt="Gaming controller" class="game-img">
-                </div>
-                <div class="col-md about-col aos-init aos-animate" data-aos="fade-left" data-aos-duration="1500">
-                    <p>We are a team of students from SIT.</p>
+            <div class="about-members">
+                <h1 class="about-header">Meet the team!</h1>
+                <p class="about-author">And find out more about their favourite games...</p>
+                <div class="about-member-profiles">
+                    <div class="member">
+                        <img src="./images/profile-filler.jpg" alt="" width="120" height="120">
+                        <span class="member-name">Jing Kiang</span>
+                        <span class="member-title">Carry</span>
+                        <span class="member-game">Dota 2, Pokemon</span>
+                    </div>
+                    <div class="member">
+                        <img src="./images/profile-filler.jpg" alt="" width="120" height="120">
+                        <span class="member-name">Hakiim</span>
+                        <span class="member-title">Carry</span>
+                        <span class="member-game">Dota 2, Pokemon</span>
+                    </div>
+                    <div class="member">
+                        <img src="./images/profile-filler.jpg" alt="" width="120" height="120">
+                        <span class="member-name">Kenneth</span>
+                        <span class="member-title">Carry</span>
+                        <span class="member-game">Dota 2, Pokemon</span>
+                    </div>
+                    <div class="member">
+                        <img src="./images/profile-filler.jpg" alt="" width="120" height="120">
+                        <span class="member-name">Kang Chen</span>
+                        <span class="member-title">Carry</span>
+                        <span class="member-game">Dota 2, Pokemon</span>
+                    </div>
+                    <div class="member">
+                        <img src="./images/profile-filler.jpg" alt="" width="120" height="120">
+                        <span class="member-name">Gavin</span>
+                        <span class="member-title">Carry</span>
+                        <span class="member-game">Dota 2, Pokemon</span>
+                    </div>
                 </div>
             </div>
         </main>
